9fb44d80bbf4e4da1b00bc6fbda0330fda61c99b
Armaghan <you@example.com>
2024-08-30 11:09:01 -0700

    correct layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] , variable:"--font-inter"});
-const ibmPlexSerif = IBM_Plex_Serif ({
-subsets:["latin"],
-weight:["400", "700"],
-variable:"--font-ibm-plex-serif"
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
 })
 
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone.",
-  icons:{
-    icon:"/icons/logo.svg",
+  icons: {
+    icon: '/icons/logo.svg'
   }
 };
 
@@ -28,3 +30,36 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
+// import type { Metadata } from "next";
+// import { Inter, IBM_Plex_Serif } from "next/font/google";
+// import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] , variable:"--font-inter"});
+// const ibmPlexSerif = IBM_Plex_Serif ({
+// subsets:["latin"],
+// weight:["400", "700"],
+// variable:"--font-ibm-plex-serif"
+// })
+
+// export const metadata: Metadata = {
+//   title: "Horizon",
+//   description: "Horizon is a modern banking platform for everyone.",
+//   icons:{
+//     icon:"/icons/logo.svg",
+//   }
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+//     </html>
+//   );
+// }
